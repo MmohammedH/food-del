047306ee67ad6fe689931eb1852c2896d98bee0a
MmohammedH <131524555+MmohammedH@users.noreply.github.com>
2024-07-09 08:48:46 +0530

    Update Header.jsx

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -4,8 +4,8 @@ const Header = () => {
   return (
     <div className='header'>
         <div className="header-contents">
-            <h2>Order your favourite food here</h2>
-            <p> Indulge in a culinary adventure from the comfort of your home. Our curated menu features the finest dishes, crafted with fresh, locally-sourced ingredients by top chefs. Whether you crave comfort food or gourmet delights, we bring the best of the culinary world straight to your door. Enjoy fast, reliable delivery and savor the flavors that make every meal special. Order now and taste the difference!</p>
+            <h2>Order now and taste the difference!</h2>
+            <p> Indulge in a culinary adventure from the comfort of your home. Our curated menu features the finest dishes, crafted with fresh, locally-sourced ingredients by top chefs. Whether you crave comfort food or gourmet delights, we bring the best of the culinary world straight to your door. Enjoy fast, reliable delivery and savor the flavors that make every meal special.</p>
             <button>View Menu</button>
         </div>
     </div>
