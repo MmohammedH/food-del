9dc1437d007de821083558122e095fc955e978bc
MmohammedH <131524555+MmohammedH@users.noreply.github.com>
2024-07-17 14:39:51 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import './App.css'
 import Navbar from './components/Navbar/Navbar'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
